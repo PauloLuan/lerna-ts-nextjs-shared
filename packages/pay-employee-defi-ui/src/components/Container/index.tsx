4060bc6235e9342424090622d78533bb7bad4665
Paulo Luan <pauloluan.inova@gmail.com>
2021-05-29 07:25:17 -0300

    trying to make shared lib works

--- packages/pay-employee-defi-ui/src/components/Container/index.tsx
+++ packages/pay-employee-defi-ui/src/components/Container/index.tsx
@@ -1,16 +1,20 @@
 import { Flex, useColorMode, FlexProps } from '@chakra-ui/react'
+import { mindingo } from '@pauloluan/shared'
 
 const Container = (props: FlexProps) => {
   const { colorMode } = useColorMode()
+
+  const teste = mindingo()
+  console.log(teste)
 
   const bgColor = { light: 'gray.50', dark: 'gray.900' }
   const color = { light: 'black', dark: 'white' }
 
   return (
     <Flex
-      direction="column"
-      alignItems="center"
-      justifyContent="flex-start"
+      direction='column'
+      alignItems='center'
+      justifyContent='flex-start'
       bg={bgColor[colorMode]}
       color={color[colorMode]}
       {...props}
