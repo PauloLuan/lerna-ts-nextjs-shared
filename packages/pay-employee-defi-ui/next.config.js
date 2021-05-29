8738d1ef59aaedd1d88ebbcaaa617aa79697594d
Paulo Luan <pauloluan.inova@gmail.com>
2021-05-29 07:41:32 -0300

    trying to transpile external modules

--- packages/pay-employee-defi-ui/next.config.js
+++ packages/pay-employee-defi-ui/next.config.js
@@ -1,10 +1,17 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')
+const withTM = require('next-transpile-modules')
+
 const isProduction = process.env.NODE_ENV === 'production'
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    disable: !isProduction
-  }
-})
+module.exports = withPWA(
+  {
+    pwa: {
+      dest: 'public',
+      disable: !isProduction
+    }
+  },
+  withTM({
+    transpileModules: ['@pauloluan/shared']
+  })
+)
